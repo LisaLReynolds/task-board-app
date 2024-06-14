611f786158e9421e7566b1850600b6679ed9fcde
Bowser <lisalreynolds2015@gmail.com>
2024-06-13 21:42:18 -0600

    fixed js code for date entry and task id

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,29 +5,33 @@ const taskFormEl = $("#taskForm");
 const taskTitleInputEl = $("#task-title");
 const taskDescriptionInputEl = $("#task-description");
 const taskDateInputEl = $("#taskDueDate");
-
+const todoCardEl = $("#todo-cards");
 // modal
 const modal = document.querySelector("#theModal");
 const btn = document.querySelector(".btn");
 const span = document.querySelector(".close")[0];
 
-// Retrieve tasks and nextId from localStorage//////////////////done
+// Retrieve tasks and nextId from localStorage//////////////////
 function readTasksFromStorage() {
   let taskList = JSON.parse(localStorage.getItem("tasks"));
-  let nextId = JSON.parse(localStorage.getItem("nextId"));
+  let nextId = parseInt(localStorage.getItem("nextId"));
   if (!taskList) {
     taskList = [];
+  }
+  if (!nextId) {
+    nextId = 0;
   }
 
   return { taskList, nextId };
 }
 
-////////////////////////done
-function saveTasksToStorage(tasks) {
+////////////////////////d
+function saveTasksToStorage(tasks, newNextId) {
+  localStorage.setItem("nextId", newNextId.toString());
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-// Todo: create a function to generate a unique task id////////////////////done
+// Todo: create a function to generate a unique task id////////////////////d
 function generateTaskId() {
   const timestamp = Date.now(); // Get the current timestamp
   const randomNum = Math.floor(Math.random() * 1000); // Generate a random number between 0 and 999
@@ -37,7 +41,7 @@ function generateTaskId() {
 const taskId = generateTaskId();
 console.log(taskId);
 
-// Todo: create a function to create a task card/////////////////done
+// Todo: create a function to create a task card/////////////////
 function createTaskCard(task) {
   const taskCard = $("<div>")
     .addClass("card task-card draggable my-3")
@@ -73,10 +77,10 @@ function createTaskCard(task) {
   // ? Return the card so it can be appended to the correct lane.
   return taskCard;
 }
-
+function displayTaskData() {}
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-  const tasks = readTasksFromStorage();
+  const { taskList } = readTasksFromStorage();
 
   const todoList = $("#todo-cards");
   todoList.empty();
@@ -88,7 +92,7 @@ function renderTaskList() {
   doneList.empty();
 
   // ? Loop through projects and create project cards for each status
-  for (let task of tasks) {
+  for (let task of taskList) {
     if (task.status === "to-do") {
       todoList.append(createTaskCard(task));
     } else if (task.status === "in-progress") {
@@ -97,23 +101,10 @@ function renderTaskList() {
       doneList.append(createTaskCard(task));
     }
   }
+  console.log(todoList);
+  window.location.reload();
 
   // ? Use JQuery UI to make task cards draggable
-  $(".draggable").draggable({
-    opacity: 0.7,
-    zIndex: 100,
-    // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
-    helper: function (e) {
-      // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
-      const original = $(e.target).hasClass("ui-draggable")
-        ? $(e.target)
-        : $(e.target).closest(".ui-draggable");
-      // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
-      return original.clone().css({
-        width: original.outerWidth(),
-      });
-    },
-  });
 }
 
 // Todo: create a function to handle adding a new task////135//////done
@@ -122,22 +113,20 @@ function handleAddTask(event) {
   const taskTitle = taskTitleInputEl.val().trim();
   const taskDescription = taskDescriptionInputEl.val();
   const taskDate = taskDateInputEl.val();
-
+  const { taskList, nextId } = readTasksFromStorage();
+  let newNextId = nextId + 1;
   const newTask = {
+    id: newNextId,
     title: taskTitle,
     description: taskDescription,
     dueDate: taskDate,
     status: "to-do",
   };
 
-  const tasks = readTasksFromStorage();
-  tasks.push(newTask);
+  taskList.push(newTask);
 
   // save the updated tasks array to localStorage
-  saveTasksToStorage(tasks);
-
-  // display task data to screen
-  displayTaskData();
+  saveTasksToStorage(taskList, newNextId);
 
   // clear the form inputs
   taskTitleInputEl.val("");
@@ -145,7 +134,7 @@ function handleAddTask(event) {
   taskDateInputEl.val("");
 }
 
-// Todo: create a function to handle deleting a task/////////////done
+// Todo: create a function to handle deleting a task/////////////
 function handleDeleteTask(event) {
   const taskId = $(this).attr("data-task-id");
   const tasks = readTasksFromStorage();
@@ -185,11 +174,11 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 
-// ? Add event listener to the form element, listen for a submit event, and call the `handleProjectFormSubmit` function.
+// ? Add event listener to the form element, listen for a submit event, and call the `handleAddTask` function.
 taskFormEl.on("submit", handleAddTask);
 
 // ? Because the cards are dynamically added to the screen, we have to use jQuery event delegation to listen for clicks on the added cards delete button.
-// ? We listen for a click on the parent element, and THEN check if the target of the click is the delete button. If it is, we call the `handleDeleteProject` function
+// ? We listen for a click on the parent element, and THEN check if the target of the click is the delete button. If it is, we call the `handleDeleteTask` function
 taskDisplayEl.on("click", ".btn-delete-project", handleDeleteTask);
 
 $(document).ready(function () {
@@ -213,6 +202,6 @@ btn.onclick = function () {
 };
 
 // When the user clicks on <span> (x), close the modal
-// span.onclick = function () {
-//   modal.style.display = "none";
-// };
+span.onclick = function () {
+  modal.style.display = "none";
+};
